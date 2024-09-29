0b920c56c5427d9b25d214da044f59e13c082e9b
sidoniastanton <sidonia.stanton@maine.edu>
2024-09-29 15:23:32 -0400

    commit 3
    8/15 questions done yeehaw

--- quiz.py
+++ quiz.py
@@ -24,7 +24,7 @@ else:
 
 
 
-user_response = input("Which of these crimes did Baroque painter Caravaggio not commit? \nA: Threatening the Pope\nB. Carrying a sword without a permit\nC: Beating a man with a stick\nD: Writing satirical poems about another artist\n:")
+user_response = input("Which of these crimes did Baroque painter Caravaggio NOT commit? \nA: Threatening the Pope\nB. Carrying a sword without a permit\nC: Beating a man with a stick\nD: Writing satirical poems about another artist\n:")
 user_response = user_response.lower().strip()
 
 if (user_response == "a"):
@@ -46,8 +46,41 @@ else:
 
 
 user_response = input("Francis Bacon's lover, George Dyer, was found dead two days before the biggest art showing of Bacon's life. Dyer had died\n\
-in the same position Bacon had once painted him: ")
+in the same position Bacon had once painted him:____\n: ")
 if (user_response == "on the toilet"):
+    print("Correct!")
+    correct_answers = correct_answers +1
+else:
+    print("Incorrect")
+
+
+user_response = input("Andy Warhol once said in his diaries that having his _____ ripped off was worse than being shot.\n:")
+if (user_response == "wig"):
+    print("Correct!")
+    correct_answers = correct_answers +1
+else:
+    print("Incorrect") 
+    # side note -- i'm almost certain this is true, but i spent too long trying to check myself, so let's just pretend i'm right
+
+
+user_response = input("Which of these toxic substances was NOT historically used as pigment for paints?\nA: Lead (White)\nB: Uranium (Orange)\nC: Mercury Sulfide (Red)\nD: Deadly Nightshade (Purple)\n: ")
+if (user_response == "d"):
+    print("Correct!")
+    correct_answers = correct_answers +1
+else:
+    print("Incorrect") 
+
+
+user_response = input("Water-soluble ink (composed of black soot and a binding agent usually made from fish bones) was invented in which country in 2500 BC?\n: ")
+if (user_response == "china"):
+    print("Correct!")
+    correct_answers = correct_answers +1
+else:
+    print("Incorrect")
+
+
+user_response = input("How many years did it take Michelangelo to paint the ceiling of the Sistine Chapel?\n: ")
+if (user_response == "4" or "four"):
     print("Correct!")
     correct_answers = correct_answers +1
 else:
