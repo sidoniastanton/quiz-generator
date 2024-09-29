e7cea61d040ff0d7fa436d6b5de6e6f426ee712a
sidoniastanton <sidonia.stanton@maine.edu>
2024-09-29 14:43:10 -0400

    commit 2
    added some questions and fixed the grade calculation

--- quiz.py
+++ quiz.py
@@ -24,4 +24,49 @@ else:
 
 
 
-print(f"Percent Grade: {int(correct_answers/15)*100:<1f}%")
+user_response = input("Which of these crimes did Baroque painter Caravaggio not commit? \nA: Threatening the Pope\nB. Carrying a sword without a permit\nC: Beating a man with a stick\nD: Writing satirical poems about another artist\n:")
+user_response = user_response.lower().strip()
+
+if (user_response == "a"):
+    print("Correct!")
+    correct_answers = correct_answers +1
+else:
+    print("Incorrect")
+
+
+
+user_response = input("Michelangelo preferred ____ over painting.\n:")
+user_response = user_response.lower().strip()
+
+if (user_response == "sculpting"):
+    print("Correct!")
+    correct_answers = correct_answers +1
+else:
+    print("Incorrect")
+
+
+user_response = input("Francis Bacon's lover, George Dyer, was found dead two days before the biggest art showing of Bacon's life. Dyer had died\n\
+in the same position Bacon had once painted him: ")
+if (user_response == "on the toilet"):
+    print("Correct!")
+    correct_answers = correct_answers +1
+else:
+    print("Incorrect")
+
+
+percent_correct = (correct_answers/15)*100
+print(f"Your score: {int(correct_answers)} / 15")
+print(f"Percent Grade: {int(percent_correct):<1.0f}%")
+
+if (percent_correct<=59): 
+   grade = "F"
+elif (60 <= percent_correct <= 69):
+    grade = "D"
+elif (70<=percent_correct<=79):
+    grade = "C"
+elif (80<=percent_correct<=89):
+    grade = "B"
+elif (90<=percent_correct<=100):
+    grade = "A"
+
+print(f"Letter Grade: {grade}")
